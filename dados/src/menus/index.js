869d9e1e6ed808bb482ee9be4d3038d0fd45dfbb
Hiudy <153995129+hiudyy@users.noreply.github.com>
2025-03-05 20:46:44 -0300

    Add dados/src/menus/index.js 1741218402737

---
+++ dados/src/menus/index.js
@@ -0,0 +1,23 @@
+const menu = require('./menu.js');
+const menudown = require('./menudown.js');
+const menuadm = require('./menuadm.js');
+const menubn = require('./menubn.js');
+const menuDono = require('./menudono.js');
+const menuMembros = require('./menumemb.js');
+const menuFerramentas = require('./ferramentas.js');
+const menuSticker = require('./menufig.js');
+const menuIa = require('./menuia.js');
+const menuRpg = require('./menurpg.js');
+
+module.exports = {
+    menu,
+    menudown,
+    menuadm,
+    menubn,
+    menuDono,
+    menuMembros,
+    menuFerramentas,
+    menuSticker,
+    menuIa,
+    menuRpg
+};
