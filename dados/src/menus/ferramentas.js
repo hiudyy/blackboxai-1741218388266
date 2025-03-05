2a8b3c8fce81de4744c63eead8bf2798abf21938
Hiudy <153995129+hiudyy@users.noreply.github.com>
2025-03-05 20:46:45 -0300

    Add dados/src/menus/ferramentas.js 1741218403795

---
+++ dados/src/menus/ferramentas.js
@@ -0,0 +1,22 @@
+async function menuMembros(prefix) {
+  return `
+╭━━━━━━━━━━━━━━╮
+┃⚒️ *MENU DE FERRAMENTAS*
+╰━━━━━━━━━━━━━━╯
+
+╭──────────────╮
+│  📚 *Comandos Disponíveis*
+├──────────────┤
+│ *${prefix}gerarnick* ou *${prefix}nick*
+│ *${prefix}ssweb*
+│ *${prefix}upload*
+│ *${prefix}encurtalink*
+╰──────────────╯
+
+╭━━━━━━━━━━━━━━╮
+┃ 🌟 *Divirta-se e Explore!* 🌟
+╰━━━━━━━━━━━━━━╯
+`;
+}
+
+module.exports = menuMembros;
