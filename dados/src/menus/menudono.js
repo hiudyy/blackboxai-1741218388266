390748ffc3297f4103be0ad31883052b89ee65e0
Hiudy <153995129+hiudyy@users.noreply.github.com>
2025-03-05 20:46:39 -0300

    Add dados/src/menus/menudono.js 1741218397561

---
+++ dados/src/menus/menudono.js
@@ -0,0 +1,35 @@
+async function menuDono(prefix) {
+  return `
+╭━━━━━━━━━━━━━━╮
+┃ 🌸 *MENU DO DONO* 🌸
+╰━━━━━━━━━━━━━━╯
+
+╭──────────────╮
+│  📂 *Configurar Bot* 
+├──────────────┤
+│ *${prefix}prefixo* 
+│ *${prefix}numerodono* 
+│ *${prefix}nomedono* 
+│ *${prefix}nomebot* 
+│ *${prefix}fotomenu* 
+│ *${prefix}videomenu* 
+╰──────────────╯
+
+╭──────────────╮
+│  📂 *Funções de dono* 
+├──────────────┤
+│ *${prefix}seradm*
+│ *${prefix}sermembro*
+│ *${prefix}bangp*
+│ *${prefix}unbangp*
+│ *${prefix}addpremium*
+│ *${prefix}delpremium*
+╰──────────────╯
+
+╭━━━━━━━━━━━━━━╮
+┃ 🌸 *Explore e Divirta-se!* 🌸
+╰━━━━━━━━━━━━━━╯
+`;
+}
+
+module.exports = menuDono;
