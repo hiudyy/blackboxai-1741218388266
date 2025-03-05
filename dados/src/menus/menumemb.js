bf08ede493bb5136dd887cb9e6d6105538e599f5
Hiudy <153995129+hiudyy@users.noreply.github.com>
2025-03-05 20:46:33 -0300

    Add dados/src/menus/menumemb.js 1741218392115

---
+++ dados/src/menus/menumemb.js
@@ -0,0 +1,25 @@
+async function menuMembros(prefix) {
+  return `
+╭━━━━━━━━━━━━━━╮
+┃ 🌟 *MENU DE MEMBROS*
+╰━━━━━━━━━━━━━━╯
+
+╭──────────────╮
+│  📚 *Comandos Gerais*
+├──────────────┤
+│ *${prefix}ajuda*
+│ *${prefix}mention*
+│ *${prefix}ping*
+│ *${prefix}rvisu*
+│ *${prefix}totalcmd*
+│ *${prefix}rankativo*
+│ *${prefix}rankinativo*
+╰──────────────╯
+
+╭━━━━━━━━━━━━━━╮
+┃ 🌟 *Divirta-se e Explore!*
+╰━━━━━━━━━━━━━━╯
+`;
+}
+
+module.exports = menuMembros;
