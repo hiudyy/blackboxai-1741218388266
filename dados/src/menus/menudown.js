0d71f3edafbe107101e240ff3a58158e3baa0b1f
Hiudy <153995129+hiudyy@users.noreply.github.com>
2025-03-05 20:46:37 -0300

    Add dados/src/menus/menudown.js 1741218395939

---
+++ dados/src/menus/menudown.js
@@ -0,0 +1,30 @@
+async function menudown(prefix) {
+  return `
+╭━━━━━━━━━━━━━━╮
+┃📥 *MENU DE DOWNLOADS*
+╰━━━━━━━━━━━━━━╯
+
+╭──────────────╮
+│  🔍 *Comandos de Pesquisas* 
+├──────────────┤
+│ *${prefix}play*
+│ *${prefix}playvid*
+│ *${prefix}assistir*
+│ *${prefix}mcplugin*
+╰──────────────╯
+
+╭──────────────╮
+│  📲 *Comandos de Downloads* 
+├──────────────┤
+│ *${prefix}tiktok* ou *${prefix}ttk*
+│ *${prefix}pinterest* ou *${prefix}pin*
+│ *${prefix}instagram* ou *${prefix}ig*
+╰──────────────╯
+
+╭━━━━━━━━━━━━━━╮
+┃ 📥 *Aproveite os downloads!*
+╰━━━━━━━━━━━━━━╯
+`;
+};
+
+module.exports = menudown;
